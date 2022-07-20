5ccd756c87e9bfd3ae102b1eb38e7deffacb67a7
TommyUeno <ueno.zmro@gmail.com>
2022-07-20 16:47:58 +0900

    GotoTitlecsを改良しました

---
+++ Fallmen/Assets/MyProject/Scripts/GotoTitle.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GotoTitle : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Invoke("ChangeScene", 1.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void ChangeScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
