c39520ae62dbc0ffa550353fc4007b98293bbb05
TommyUeno <ueno.zmro@gmail.com>
2022-08-04 00:22:14 +0900

    Robie/テキストの文字送りを実装しました。（文に関してはまだ仮）

---
+++ Fallmen/Assets/MyProject/Scripts/NovelScript.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class NovelScript : MonoBehaviour
+{
+    [SerializeField] List<string> messageList = new List<string>();
+    [SerializeField] TextMeshProUGUI text;
+    [SerializeField] float novelSpeed;
+    int novelListIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Novel());
+    }
+
+    private IEnumerator Novel()
+    {
+        int messageCount = 0;
+        text.text = "";
+        //textaudio.Play();
+
+        while (messageList[novelListIndex].Length > messageCount)
+        {
+            text.text += messageList[novelListIndex][messageCount];
+            messageCount++;
+
+            yield return new WaitForSeconds(novelSpeed);
+
+        }
+
+        novelListIndex++;
+        if(novelListIndex < messageList.Count)
+        {
+            yield return new WaitForSeconds(0.2f);
+            yield return new WaitUntil(() => Input.GetKey(KeyCode.Space));//OVRInput.GetDown(OVRInput.RawButton.B));
+            StartCoroutine(Novel());
+        }
+        else
+        {
+            yield return new WaitForSeconds(0.2f);
+            yield return new WaitUntil(() => Input.GetKey(KeyCode.Space));//OVRInput.GetDown(OVRInput.RawButton.B));
+
+
+        }
+    }
+}
