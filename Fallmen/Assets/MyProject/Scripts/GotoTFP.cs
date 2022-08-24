a663d437a2440fa7fd73282551fcdf6c44e75f74
TommyUeno <ueno.zmro@gmail.com>
2022-08-24 12:07:03 +0900

    Result/シーン遷移CSを書いて、環境を整えました。

---
+++ Fallmen/Assets/MyProject/Scripts/GotoTFP.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GotoTFP : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("ChangeScene", 10f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void ChangeScene()
+    {
+        SceneManager.LoadSceneAsync("ThanksForPlaing");
+    }
+}
